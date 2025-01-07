66c73b212e843b22a94b9024fbc04d1cec54ffa8
hungnq131002 <hungnq131002@gmail.com>
2025-01-07 09:02:29 +0700

    if conditional

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -2,16 +2,50 @@ import { Fragment } from "react";
 
 function ListGroup() {
 
-  const items = [
+  let items = [
     'An item',
     'A second item',
     'A third item',
     'A fourth item',
     'And a fifth one'
   ];
+  items = []; // test with empty array
 
   // not have loop in JSX
+
+  // conditional rendering
+  // #1
+  // if (items.length === 0)
+  //   //but loop h1 is not a good practice 
+  //   return <Fragment>
+  //     <h1>List</h1> 
+  //     <p>There are no items</p>
+  //   </Fragment>;
+
+  // #2
+  // add if directly in JSX is not a good practice, too
+  // <Fragment>
+  //     <h1>List Group</h1>
+  //     {(items.length === 0) ? <p>There are no items</p> : null}
+  //     <ul className="list-group">
+  //       {items.map((item) => (
+  //         <li key={item}>{item}</li>
+  //       ))}
+  //     </ul>
+  //   </Fragment>
   
+  // #3
+  // use const or function to store conditional rendering
+  const message = items.length === 0 ? <p>There are no items</p> : null;
+  // or use && operator
+  // const message = items.length === 0 && <p>There are no items</p>;
+  
+  // benefit: function can pass parameter 
+  // const getMessage() {
+  //   if (items.length === 0) {
+  //     return <p>There are no items</p>;
+  //   }
+  //   return null;
 
   return (
     // cannot create multiple elements in JSX
@@ -35,6 +69,7 @@ function ListGroup() {
     // or use a div (add extra div element)
     <Fragment>
       <h1>List Group</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
