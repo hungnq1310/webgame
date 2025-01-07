8abdd425546150d15a3ba65a0e72956126a86c4a
hungnq131002 <hungnq131002@gmail.com>
2025-01-07 08:47:01 +0700

    hello world

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+// JS class or function 
+
+// PascalCasing - ComponentName
+function Message() {
+    const name = '';
+    if (name)
+        return <h1>Hi, I am a {name}</h1>;
+    // React.createElement('h1', null, 'Hi, I am a message') - Javascript XML
+    return <h1>Hi, I am a message</h1>;
+}
+
+export default Message;
